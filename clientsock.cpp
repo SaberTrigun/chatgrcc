6f734bcf910d3d29986d81cc4951813af94b4f2d
vaorlov <00700590@tutanota.com>
2020-04-06 15:12:37 +0300

    i am just try

--- clientsock.cpp
+++ clientsock.cpp
@@ -7,50 +7,37 @@
 using namespace std;
 
 int main(){
-	int sockfd, new_sockfd, yes=1, snd;
+	int sockfd, new_sockfd, yes=1, snd, rcv;
 	const int PORT = 7890;
-	char buf[1024];
+	char buf[6];
 	struct sockaddr_in host_addr, client_addr;
 	socklen_t sin_size;
-	char send_msg[1024];
-	send_msg[0]='f';
-	send_msg[1]='u';
-	send_msg[2]='c';
-	send_msg[3]='k';
-	send_msg[4]='	';
-
+	
 	if ((sockfd = socket(PF_INET, SOCK_STREAM, 0))!=-1)
 		cout << "sockfd create......" << sockfd << endl;
 
 	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int))!=-1)
 		cout << "setsockopt accepted......" << setsockopt << endl;
 
-	host_addr.sin_family=AF_INET;
-	host_addr.sin_port=htons(PORT);
-	host_addr.sin_addr.s_addr=0;
+	client_addr.sin_family=AF_INET;
+	client_addr.sin_port=htons(PORT);
+	client_addr.sin_addr.s_addr=0;
 
 	memset(&(host_addr.sin_zero), '\0', 8);
 	
 	if (bind(sockfd, (struct sockaddr *)&host_addr, sizeof(struct sockaddr))!=-1)
 		cout << "bind addr_iface and num_port..." << bind << endl;
 
-	if (listen(sockfd, 5)!=-1)
-		cout << "waiting connections......" << listen << endl;
+	if ((new_sockfd=connect(sockfd, (struct sockaddr *)&client_addr, sizeof(client_addr)))==0)
+		cout<<"Connected!"<<endl;
+	rcv=recv(new_sockfd, &buf, sizeof(buf), 0);
+	cout <<rcv<<endl;
 
-	while(1)
+int i=0;
+	while(rcv>=0)
 	{
-		sin_size=sizeof(struct sockaddr_in);
-		new_sockfd=accept(sockfd, (struct sockaddr *)&client_addr, &sin_size);
-		if (new_sockfd!=-1)
-			cout << "connecting......" << endl;
-		cout << "incoming connection" << endl;
-		for(int i=0; i<=5; i++)
-		snd=send(new_sockfd, &send_msg[i], 1, 0);
-
-
-
-
-
-		close (new_sockfd);
+		cout<<buf[i]<<endl;
+		i++;
 	}
+	close(new_sockfd);
 }
