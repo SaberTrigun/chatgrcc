fc7f0544de6eceb16c4416b6a3cc2ee230472ee0
trigun <00700590@tutanota.com>
2020-12-03 10:10:22 +0300

    refactoring all code

--- clientsock.cpp
+++ clientsock.cpp
@@ -1,3 +1,5 @@
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <iostream>
 #include <string.h>
 #include <sys/socket.h>
@@ -12,40 +14,38 @@
 using namespace std;
 
 //Функция приёма сообщений
-string f_recv(int sockfd, int new_sockfd){
-	char buf[1024];
-	new_sockfd=recv(sockfd, buf, 30, 0);
-	for (int i=0; i<=30; i++)
-		return(buf);	
+string f_recv(int sockfd, int new_sockfd) {
+	char	buf[1024];
+	new_sockfd = recv(sockfd, buf, 30, 0);
+	for (int i=0; i<=33; i++)
+		return(buf);
 	cout << endl;
 }
 
-//Функция отправки сообщений
-bool f_send(int sockfd, int new_sockfd, string message){
-	bool flag;
+//функция отправки сообщений
+bool f_send(int sockfd, int new_sockfd, string message) {
+	bool	flag;
 	new_sockfd = send(sockfd, message.c_str(), message.size(), 0);
 	if (new_sockfd > 0)
-		return(flag=1);
+		return(flag = 1);
 	else
-		return(flag=0);
+		return(flag = 0);
 }
 
+//Функция для отправки файла
 
 
-int main(int argc, char *in_argv[]){
+int main() {
 	int			sockfd, new_sockfd, yes=1, snd, rcv;
-	const int 		PORT = 7890;
-	char 			buf[1024];
-	struct sockaddr_in 	host_addr, client_addr, dest_srv_addr;
-	socklen_t 		sin_size;
-	string 			message;
-	bool 			flag_send;	
+	const int		PORT = 7890;
+	char			buf[1024];
+	struct sockaddr_in	host_addr, client_addr, dest_srv_addr;
+	socklen_t		sin_size;
+	string			message;
+	bool			flag_send;
 	struct hostent		*srvName;
 
-	if ((sockfd = socket(PF_INET, SOCK_STREAM, 0))!=-1)
-		cout << "sockfd create......" << sockfd << endl;
-
-
+//Пытаемся получить адрес сервера по имени datagram.ru
 	if (in_argv[1]!=NULL)
 		srvName=gethostbyname(in_argv[1]);
 	else if (in_argv[1]==NULL)
@@ -56,42 +56,36 @@ int main(int argc, char *in_argv[]){
 		cout << "Failed to get server address" << endl;
 		exit(1);
 	}
-	
+//Выводим отладочную информацию
 	cout << "Name:         " << srvName -> h_name << endl;
 	cout << "IP address:   " << inet_ntoa(*(struct in_addr*)srvName -> h_addr) << endl;
 	cout << "Type address: " << srvName -> h_addrtype << endl;
-
-
+//Приводим адрес и порт к нужному виду
 	client_addr.sin_family = srvName -> h_addrtype;
 	client_addr.sin_port   = htons(PORT);
 	inet_aton(inet_ntoa(*(struct in_addr*)srvName -> h_addr), &client_addr.sin_addr);
 	memset(&(host_addr.sin_zero), '\0', sizeof(host_addr.sin_zero));
-	
 
 
-
-	new_sockfd=connect(sockfd, (struct sockaddr *)&client_addr, sizeof(client_addr));
+	if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) != -1) //Создаём сокет
+		cout << "sockfd create......" << sockfd << endl;
+	new_sockfd = connect(sockfd, (struct sockaddr *)&client_addr, sizeof(client_addr)); //Отправляем соединение на адрес сервера
 	if (new_sockfd)
 	{
 		perror("error");
 		exit(1);
 	}
 
-
-	cout << f_recv(sockfd, new_sockfd) << endl;
-
+	cout << f_recv(sockfd, new_sockfd) << endl; //После коннекта с сервером принимаем приветственное сообщение
 
 	while (1)
 	{
-
 		getline(cin, message);
-		flag_send = f_send(sockfd, new_sockfd, message);
-		if (flag_send > 0)
-		{
+		flag_send = f_send(sockfd, new_sockfd, message);// Вызываем функцию отправки сообщения передаём дескриптор_сокета, дескриптор_соединения и сообщение. Функция вернёт флаг 1 если сообщение отправлено 
+		if (flag_send == 0) //Если флаг = 0 то сообщение отправлено
 			cout << "Message sent!" << endl;
-		}	
-		flag_send = 0;	
+		flag_sent = 0;//Обнуляем флаг 
 	}
-
-	 
 }
+
+
--- sockchat.cpp
+++ sockchat.cpp
@@ -11,92 +11,99 @@
 #include <netdb.h>
 #include <unistd.h>
 #include <limits.h>
-using namespace std;
+using namespace std; // так лучше не делать
 
 //Функция записи истории в файл
-void f_writeFile(const char *data_buffer){
+//Эту функцию со временем переписать для ухода от библиотеки fstream
+void f_writeFile(const char *data_buffer) {
 	setlocale(LC_ALL, "Russian");
-	ofstream fs;
+	fstream	fs;
 	fs.open("chat_history.log", ofstream::app);
 	if(!fs.is_open())
-		cout<<"Error open"<<endl;	
-	fs << data_buffer<<"\n" ;
-	int test=5;
+		cout << "Error open" << endl;
+	fs << data_buffer << "\n";
 	fs.close();
-
 }
 
-//Функция чтения из лог файла, непомню зачем, потом использую её
-string f_readFlile(string *cmd_history){
+//Функция чтения из лог файла. Пока не используется
+//Со временем переписать её для ухода от библиотеки fstream
+string f_readFile(string *cmd_fistory) {
 	string history;
 	setlocale(LC_ALL, "Russian");
-        ifstream fs;
-        fs.open("chat_history.log", ofstream::app);
+	ifstream fs;
+	fs.open("chat_history.log", ofstream::app);
 	if(!fs.is_open())
-                cout<<"Error open"<<endl;
-	
+		cout << "Error_open" << endl;
 	return history;
-
 }
 
-int main(){
+int main () {
+	
 	int			sockfd, new_sockfd, yes=1, snd, rcv;
 	const int		PORT = 7890;
-	char			buf[1024];
-	struct sockaddr_in 	host_addr, client_addr;
+	char			buf[1024], hostname[HOST_NAME_MAX];
+	struct	sockaddr_in	host_addr, client_addr;
 	socklen_t		sin_size;
-	struct hostent		*srvName;
-	char			hostname[HOST_NAME_MAX];
+	struct hostent		*srvName;	
 
-	if ((sockfd = socket(PF_INET, SOCK_STREAM, 0))!=-1)
-		cout << "sockfd create......" << sockfd << endl;
-
-	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int))!=-1)
-		cout << "setsockopt accepted......" << setsockopt << endl;
-
+//Получаем ip аддресс по имени 
 	gethostname(hostname, HOST_NAME_MAX);
-	srvName=gethostbyname(hostname);
-	if (srvName==NULL) {
+	srvName = gethostbyname(hostname);
+	if(srvName == NULL) {
 		herror("gethostbyname");
 		exit(1);
 	}
-	
+
+	if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) != -1) //Получаем tcp сокет типа Internet
+		cout << "sockfd create......" << sockfd << endl;
+	else
+		perror ("Error call socket");
+	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) != -1) // Установка параметров сокета 
+		cout << "setsockopt accepted......" << setsockopt << endl;
+	else
+		perror ("Error setsockopt");
+
+//Вывод отладочной информации	
 	cout << "Name:         " << srvName -> h_name << endl;
 	cout << "IP address:   " << inet_ntoa(*(struct in_addr*)srvName -> h_addr) << endl;
 	cout << "Type address: " << srvName -> h_addrtype << endl;
-
+//Приведение адреса и порта к нужному виду
 	host_addr.sin_family = srvName -> h_addrtype;
 	host_addr.sin_port   = htons(PORT);
 	inet_aton(inet_ntoa(*(struct in_addr*)srvName -> h_addr), &host_addr.sin_addr);
 	memset (&(host_addr.sin_zero), '\0', sizeof(host_addr.sin_zero));
-	
 
-	if (bind(sockfd, (struct sockaddr *)&host_addr, sizeof(struct sockaddr))!=-1)
+
+	if (bind(sockfd, (struct sockaddr *)&host_addr, sizeof(struct sockaddr))!=-1) //Биндим сокет
 		cout << "bind addr_iface and num_port..." << bind << endl;
-
-	if (listen(sockfd, 5)!=-1)
+	else
+		perror ("Error bind");
+	if (listen(sockfd, 5)!=-1) // Прослушиваем
 		cout << "waiting connections......" << listen << endl;
+	else
+		perror ("Error listen");
 
 
-
-	while(1)
+	while (1)
 	{
-		sin_size=sizeof(struct sockaddr_in);
-		new_sockfd=accept(sockfd, (struct sockaddr *)&client_addr, &sin_size);
-		if (new_sockfd!=-1)
-			cout << "connecting......" << endl << "incoming connection" << endl;
-		send(new_sockfd, "Hy, welcome to grcc.com!!! O_0", 19, 0); cout<<send<<endl;
-		rcv=recv(new_sockfd, &buf, 1024, 0);
-		while (rcv>0)
+		sin_size = sizeof(struct sockaddr_in);
+		new_sockfd = accept(sockfd, (struct sockaddr *)&client_addr, &sin_size); //Принимаем входящее соединение
+		if (new_sockfd != -1)
+			cout << "Connecting......" << endl << "incoming connection" << endl;
+		send(new_sockfd, "Hy, welcome to datagram.com!!! 0_0", 34, 0); //Отправляем приветственное сообщене клиенту
+		rcv = recv(new_sockfd, &buf, 1024, 0);
+		while (rcv > 0)
 		{
 			printf("RECV: %d байтов\n", rcv);
 			dump(buf, rcv);
-			f_writeFile(buf); // Запись истории в файл
-			rcv=recv(new_sockfd, buf, 1024, 0);
-		}
-
-
-
+			f_writeFile(buf);//Запись истории в файл
+			rcv = recv(new_sockfd, buf, 1024, 0);
+		}	
 		close (new_sockfd);
+
 	}
+
+
+
 }
+
